748f39aa06b0e7f6037c87a137ff6b4912f51de0
Olivia Vasquez <liverbeans@users.noreply.huggingface.co>
2026-08-11 17:21:43 +0000

    Update app.py
    Changed Max tokens and added temperature

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from huggingface_hub import InferenceClient
 # (Settings -> Variables and secrets -> New secret).
 
 client = InferenceClient("Qwen/Qwen2.5-7B-Instruct", bill_to="kode-with-klossy")
-
+temperture = 2.0
 
 def respond(message, history):
     messages = [{"role": "system", "content": "You are a friendly chatbot."}]
@@ -23,7 +23,8 @@ def respond(message, history):
 
     response = client.chat_completion(
         messages,
-        max_tokens=100
+        max_tokens=20
+        temperature=temperature
     )
 
     return response.choices[0].message.content.strip()
