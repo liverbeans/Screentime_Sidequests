dacbcd1884dd4227cf211bef927104abdbdf9835
Nidhi Soni <nidhii33@users.noreply.huggingface.co>
2026-08-13 21:09:27 +0000

    Update app.py
    edited the desc

--- app.py
+++ app.py
@@ -130,7 +130,7 @@ chatbot = gr.ChatInterface(respond,
    
      title="｡  🎀  𝒮𝒸𝓇𝑒𝑒𝓃𝓉𝒾𝓂𝑒 𝒮𝒾𝒹𝑒𝓆𝓊𝑒𝓈𝓉𝓈  🎀  ｡",
                            
-    description = "Input your age, interests, and time you want to spend off your screen! You can do this in the text box if you want to be more specific, or you can click the checkboxes by opening up the additional inputs ", 
+    description = "Input your age, interests, and time you want to spend off your screen! You can do this in the text box if you want to be more specific with things like your budget, or you can click the checkboxes by opening up the additional inputs! ", 
     
     additional_inputs=[ gr.CheckboxGroup (["shopping", "art","sports/working out", "cooking"," reading","video games", "music","writing"],label="activities"), 
     gr.CheckboxGroup (["thirty minutes","one hour","two hours","three hourse","four hours","five+ hours"],label="time"),
